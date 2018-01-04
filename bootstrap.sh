e4efc0b5989dc6ef89d90d50a049152d1df17ae5
George Crary <craryg@onid.oregonstate.edu>
2018-01-04 05:17:47 -0800

    Add fishshell related configuration and bootstrap.sh for fresh install

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# dotfiles from https://github.com/adpextwindong/dotfiles
+
+echo "Copying files to home dir"
+//TODO add case for if folders don't exist
+mv .bashrc ~/
+//mv .config/fish/config.fish ~/
