14db5ecf8e4573869fd27a724aec90ff60b9918f
Takumi Crary <craryg@oregonstate.edu>
2018-04-09 23:30:15 -0700

    Fix incorrect bash comments in bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 # dotfiles from https://github.com/adpextwindong/dotfiles
-
 echo "Copying files to home dir"
-//TODO add case for if folders don't exist
+
+#TODO add case for if folders don't exist
 mv .bashrc ~/
-//mv .config/fish/config.fish ~/
+
+#mv .config/fish/config.fish ~/
