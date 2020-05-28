8fdd03c5659b79bd4f0e2c2131572d97d38021a0
Takumi Crary <tcrary@gmail.com>
2020-05-28 01:05:09 +0000

    Add LF ending and autocrlf input configs for git

--- config_git.sh
+++ config_git.sh
@@ -3,3 +3,5 @@ git config --global user.email "tcrary@gmail.com"
 git config --global user.name "Takumi Crary"
 git config --global core.editor "vim"
 git config --global push.default simple
+git config --global core.eol lf
+git config --global core.autocrlf input
